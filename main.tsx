c0be640e3c298fe2736b5d94872b55d71a93b190
Elproximohit <redsroyalbooking@gmail.com>
2025-12-10 06:24:42 -0400

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,15 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("No se encontró el elemento root");
-}
-
-ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Este archivo es obsoleto. El punto de entrada es index.tsx en la raíz.
