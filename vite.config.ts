635276ddf07efffdd1a53c5580da1ddfce5d36ad
Elproximohit <redsroyalbooking@gmail.com>
2025-12-12 18:20:00 +0000

    Arreglado base path para Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    plugins: [react()],
-    root: '.', // Importante: define la raíz del proyecto explícitamente
-    build: {
-      outDir: 'dist',
-    },
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.API_KEY || "") 
-    }
-  }
+export default defineConfig({
+  base: '/', // <- ruta raíz para Vercel y dominio propio
+  plugins: [react()],
 })
